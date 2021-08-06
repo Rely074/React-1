66da06e6eb0148a122cf72af90e24abf78bb6110
Denis <natsy074@gmail.com>
2021-08-07 00:06:58 +0300

    урок 7

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import React from "react";
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-// import Chat from "../src/components/Chat/Chat"
+import {useDispatch, useSelector} from "react-redux";
+import {changeName} from "../../actions/profile";
+import Input from "../Input/Input";
 
 
 function App() {
@@ -11,30 +13,47 @@ function App() {
         { id: 'chat2', name: 'Чат 2' },
         { id: 'chat3', name: 'Чат 3' },
     ])
+
+    const dispatch = useDispatch()
+    const {name,age} = useSelector(state => state.profile)
+
     const [currentChat, setCurrentChat] = React.useState(chats[0])
     const handleChangeChat = (chat) => setCurrentChat(chat)
 
+    const handleNameSubmit = (newName) => {
+        console.log("call action with", newName)
+        dispatch(changeName(newName))
+    }
+
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <div className={"chatMenu"}>
-                    <List className="app__sidebar" subheader="Список чатов">
-                        {chats.map((chat) => (
-                            <ListItem
-                                button
-                                key={chat.id}
-                                selected={chat.id === currentChat.id}
-                                onClick={() => handleChangeChat(chat)}
-                            >
-                                {chat.name}
-                            </ListItem>
-                        ))}
-                    </List>
-                </div>
-                {/*<Chat />*/}
-            </header>
-        </div>
+        <>
+            <div className="App">
+                <header className="App-header">
+                    <div className={"chatMenu"}>
+                        <List className="app__sidebar" subheader="Список чатов">
+                            {chats.map((chat) => (
+                                <ListItem
+                                    button
+                                    key={chat.id}
+                                    selected={chat.id === currentChat.id}
+                                    onClick={() => handleChangeChat(chat)}
+                                >
+                                    {chat.name}
+                                </ListItem>
+                            ))}
+                        </List>
+                    </div>
+
+                    <div className="bordered">
+                        <p><b>Name:</b>{name}</p>
+                        <p><b>Age:</b>{age}</p>
+                        <Input onSubmit={handleNameSubmit}/>
+                    </div>
+                    {/*<Chat />*/}
+                </header>
+            </div>
+        </>
     );
 }
 
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import { changeIsOnline, changeIsOnlineWithThunk } from '../../actions/profile'
 
-const Profile = (props) => {
+export default function Profile(props) {
+    const dispatch = useDispatch()
+    const { age, name, isOnline } = useSelector((state) => state.profile)
+
+    const handleIsOnlineChange = (event) => {
+        dispatch(changeIsOnlineWithThunk(event.target.checked))
+    }
+
     return (
-        <div className="Profile">
+        <div>
+            <p>Profile page</p>
+            <p>
+                <b>Name: </b>
+                {name}
+            </p>
+            <p>
+                <b>Age: </b>
+                {age}
+            </p>
 
+            <FormControlLabel
+                control={
+                    <Checkbox
+                        checked={isOnline}
+                        onChange={handleIsOnlineChange}
+                        name="checkedB"
+                        color="primary"
+                    />
+                }
+                label={<p>Is user online</p>}
+            />
         </div>
-
     )
 }
-
-export default Profile;
