d421d41f73d29323ab41ae104aa9cae40a3f21aa
Denis <natsy074@gmail.com>
2021-08-12 23:49:14 +0300

    урок 8

---
+++ src/components/App/constants.js
@@ -0,0 +1,7 @@
+export const AUTHORS = {
+    ME: 'Me',
+    BOT: 'Bot',
+}
+
+// export const API_URL = "https://app.pixelencounter.com/api/basic/monsters/random/{format}?size=int"
+export const API_URL = "https://thatcopy.pw/catapi/rest/"
