4e34a379cb7f82cc93d40b1d7c2f167169937696
Denis <natsy074@gmail.com>
2021-08-13 19:55:20 +0300

    урок 9

---
+++ src/components/Home/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Input from '../Input/Input'
+
+export default function Home(props) {
+    const { age = 0, name = 'Unknown', onChangeProfileName } = props
+
+    const handleNameSubmit = (newName) => {
+        onChangeProfileName(newName)
+    }
+
+    return (
+        <div className="app app__content app__content_row">
+            <div className="bordered">
+                <p>
+                    <b>Name: </b>
+                    {name}
+                </p>
+                <p>
+                    <b>Age: </b>
+                    {age}
+                </p>
+            </div>
+            <Input
+                label="Имя"
+                placeholder="Введите новое имя"
+                onSubmit={handleNameSubmit}
+            />
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
-import './index.css';
-import App from './components/App/App';
-import Router from "./components/Router/Router";
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import { Provider } from 'react-redux'
+import { persistor, store } from './store'
+import App from './components/App/App'
+import { PersistGate } from 'redux-persist/integration/react'
+import "./services/firebase_"
 
 ReactDOM.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <App />
-          <Router />
-      </BrowserRouter>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate persistor={persistor} loading={<div>Loading...</div>}>
+                <BrowserRouter>
+                    <App />
+
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
